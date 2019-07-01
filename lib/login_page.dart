1dd2d497cf55e4bf21e2e0df1fbcbded7725a075
Dawid <contact@dawidstefaniak.com>
2019-07-01 16:54:15 +0100

    separated main function from main page

---
+++ lib/login_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  LoginPage({Key key}) : super(key: key);
+
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+       child: Container(
+         
+       ),
+    );
+  }
+}
